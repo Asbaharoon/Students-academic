8b5dd0c2af95e453c1e66b84d68d5d97b15a6367
dawande <atharva.dawande603@gmail.com>
2019-09-28 00:46:45 +0530

    login activity update:active button sign in not log in

--- app/src/main/java/com/example/gfmcheck/dataact.java
+++ app/src/main/java/com/example/gfmcheck/dataact.java
@@ -2,16 +2,38 @@ package com.example.gfmcheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class dataact extends AppCompatActivity {
     private EditText username;
     private EditText password;
+    private Button signin;
+    private Button login;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dataact);
+
+
+        username=(EditText)findViewById(R.id.editText);
+        password=(EditText)findViewById(R.id.editText2);
+        signin=(Button) findViewById(R.id.signupbutton);
+        login=(Button) findViewById(R.id.loginpagebutton);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(dataact.this,MainActivity.class);
+                startActivity(intent1);
+            }
+        });
     }
 }
