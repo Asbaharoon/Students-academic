bda07c6ad2c8afa5edc8864cb8e9f0e8b19d33e8
dawande <atharva.dawande603@gmail.com>
2019-10-08 17:53:53 +0530

    attendence activity and setting activity

---
+++ app/src/main/java/com/example/gfmcheck/attendenceact.java
@@ -0,0 +1,14 @@
+package com.example.gfmcheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class attendenceact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendenceact);
+    }
+}
