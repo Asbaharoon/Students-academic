e03f78272c32685af30252e5dad8fa4cdbc2aa3d
dawande <atharva.dawande603@gmail.com>
2019-10-08 18:59:41 +0530

    all activities done

--- app/src/main/java/com/example/gfmcheck/dataact.java
+++ app/src/main/java/com/example/gfmcheck/dataact.java
@@ -2,7 +2,9 @@ package com.example.gfmcheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class dataact extends AppCompatActivity {
@@ -18,7 +20,21 @@ public class dataact extends AppCompatActivity {
     attendence1=(Button)findViewById(R.id.attendence);
     setting1=(Button)findViewById(R.id.setting);
 
-    attendence1.
+    attendence1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent5= new Intent(dataact.this,attendenceact.class);
+            startActivity(intent5);
+        }
+    });
+
+        setting1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent6= new Intent(dataact.this,settingact.class);
+                startActivity(intent6);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/gfmcheck/settingact.java
+++ app/src/main/java/com/example/gfmcheck/settingact.java
@@ -2,13 +2,39 @@ package com.example.gfmcheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class settingact extends AppCompatActivity {
+
+    private Button attendence3;
+    private Button form3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settingact);
+
+    attendence3=(Button)findViewById(R.id.attendences);
+    form3=(Button)findViewById(R.id.forms);
+
+    attendence3.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent7=new Intent(settingact.this,attendenceact.class);
+            startActivity(intent7);
+        }
+    });
+
+    form3.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+                Intent intent8=new Intent(settingact.this,dataact.class);
+                startActivity(intent8);
+        }
+    });
+
     }
 }
