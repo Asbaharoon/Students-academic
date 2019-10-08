56c6315ecb4f2bc576caa964b0cebe9385cfcda7
dawande <atharva.dawande603@gmail.com>
2019-10-09 00:40:52 +0530

    database connect main act

--- app/src/main/java/com/example/gfmcheck/MainActivity.java
+++ app/src/main/java/com/example/gfmcheck/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.gfmcheck;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.opengl.Visibility;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,13 +33,16 @@ public class MainActivity extends AppCompatActivity {
     private EditText conpwd;
     private Button signup;
     private Button backtologin;
-
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         f=(EditText)findViewById(R.id.f);
         m=(EditText)findViewById(R.id.m);
         l=(EditText)findViewById(R.id.l);
@@ -75,8 +89,38 @@ public class MainActivity extends AppCompatActivity {
     else{
 
         if (pwd1n.equals(conpwdn) && pwd1n!=null && conpwdn!=null) {
-            Intent intent1 = new Intent(MainActivity.this,dataact.class);
-            startActivity(intent1);
+
+            // Create a new user with a first and last name
+            Map<String, Object> user = new HashMap<>();
+            user.put("first",fn);
+            user.put("middle",mn);
+            user.put("last",ln);
+            user.put("email",ein);
+            user.put("zprn",zprnn);
+            user.put("pwd",pwd1n);
+            user.put("confirmpwd",conpwdn);
+
+// Add a new document with a generated ID
+            db.collection("users")
+                    .add(user)
+            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Toast.makeText(getApplicationContext(),"Successfull",Toast.LENGTH_SHORT);
+                           // Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            Intent intent1 = new Intent(MainActivity.this,dataact.class);
+                            startActivity(intent1);
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(),"Error adding document try again",Toast.LENGTH_LONG);
+                            // Log.w(TAG, "Error adding document", e);
+                        }
+                    });
+
         }
         else{
             conpwd.setError("*Enter same password as above");
