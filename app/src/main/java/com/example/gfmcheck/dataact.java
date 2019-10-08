bc7c6753c338a897d93ebde47978c3d1920f038c
dawande <atharva.dawande603@gmail.com>
2019-10-08 18:04:15 +0530

    attendence activity and setting activity ,,id changed for each button

--- app/src/main/java/com/example/gfmcheck/dataact.java
+++ app/src/main/java/com/example/gfmcheck/dataact.java
@@ -3,12 +3,22 @@ package com.example.gfmcheck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class dataact extends AppCompatActivity {
+
+    private Button attendence1;
+    private Button setting1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dataact);
+
+    attendence1=(Button)findViewById(R.id.attendence);
+    setting1=(Button)findViewById(R.id.setting);
+
+    attendence1.
+
     }
 }
